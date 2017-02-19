5ac3e4971115a1b268d9939175fcdad808a24854
Reece Hudson <reecehudson10@gmail.com>
2017-02-18 21:47:20 -0600

    no dot when afterDot=0

--- bin.js
+++ bin.js
@@ -91,6 +91,7 @@ function put( info ) {
 }
 
 function byteToHuman( bytes, afterDot ) {
+	afterDot = Number(afterDot);
 	bytes = +bytes;
 
 	let units = [ "b", "kb", "mb", "gb", "tb", "?", "??", "???", "????" ];
@@ -102,7 +103,12 @@ function byteToHuman( bytes, afterDot ) {
 	bytes = String( bytes );
 	dotInd = bytes.indexOf( "." );
 
-	if ( ~~dotInd ) bytes = bytes.slice( 0, dotInd + afterDot +1 );
-
+	if ( ~dotInd ) {
+		if (afterDot === 0) {
+			bytes = bytes.slice(0, dotInd);
+		} else {
+			bytes = bytes.slice( 0, dotInd + afterDot +1 );
+		}
+	}
 	return "" + bytes + units[ count ];
 }
